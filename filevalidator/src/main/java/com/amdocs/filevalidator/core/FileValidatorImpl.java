a7b1c3165e1a1ceabf18a497536e1927919e318f
rotemnaar <rotemnaar@gmail.com>
2011-02-09 19:45:21 +0000

    1. change "validate()" signature - pass file name as an argument 2. file-name module - initial version - only file length validation

--- filevalidator/src/main/java/com/amdocs/filevalidator/core/FileValidatorImpl.java
+++ filevalidator/src/main/java/com/amdocs/filevalidator/core/FileValidatorImpl.java
@@ -47,7 +47,7 @@ public class FileValidatorImpl implements FileValidator {
 		// iterate all modules
 		for (Module module : config.getModules()) {
 			logger.debug("Validating module {}", module.getName());
-			if (!module.validate(is)) { 
+			if (!module.validate(is, file.getAbsolutePath(), file.getName())) { 
 				return false;
 			}
 		}
