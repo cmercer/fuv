46c696ca5f82494955cb4aba56043098bfab5ba2
rotemnaar@gmail.com <rotemnaar@gmail.com@8c1c528d-0225-3294-f370-991fec7a0357>
2011-02-18 14:08:19 +0000

    fix File size exception

--- trunk/filevalidator/src/main/java/com/amdocs/filevalidator/exceptions/FileSizeException.java
+++ trunk/filevalidator/src/main/java/com/amdocs/filevalidator/exceptions/FileSizeException.java
@@ -1,6 +1,8 @@
 package com.amdocs.filevalidator.exceptions;
 
-public class FileSizeException extends RuntimeException {
+import java.io.IOException;
+
+public class FileSizeException extends IOException {
 
 	private static final long serialVersionUID = -8432146390083099414L;
 	
