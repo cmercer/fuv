f7f4c7c380792be363923c93e6f87059202725db
rotemnaar <rotemnaar@gmail.com>
2011-02-26 16:36:21 +0000

    Fix import

--- filevalidator/src/main/java/com/amdocs/filevalidator/modules/FileNameModule.java
+++ filevalidator/src/main/java/com/amdocs/filevalidator/modules/FileNameModule.java
@@ -14,7 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.amdocs.filevalidator.config.CharStrip;
-import com.amdocs.filevalidator.utils.FileNameUtils;
+
 
 /**
  * Validate file name (only the simple name without the extension):
